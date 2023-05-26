5c9b71e605054273f3971a0f333dda41836df873
Lorenzo Castellino <castellino96@gmail.com>
2023-05-26 16:28:01 +0200

    Fix CI

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl https://github.com/QB64Official/qb64/releases/download/v2.1/qb64_dev_2022-09-08-07-14-00_47f5044_lnx.tar.gz
-tar -xf qb64_dev_2022-09-08-07-14-00_47f5044_lnx.tar.gz
-mv qb64_dev_2022-09-08-07-14-00_47f5044_lnx qb64
+wget https://github.com/QB64Official/qb64/releases/download/v2.1/qb64_dev_2022-09-08-07-14-00_47f5044_lnx.tar.gz -O qb64.tar.gz
+tar -xf qb64.tar.gz
+mv qb64_2022-09-08-23-38-12_47f5044_lnx qb64
 
