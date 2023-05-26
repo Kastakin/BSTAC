970e317b102336ad5bb926ff21ab3b251975a34c
Lorenzo Castellino <castellino96@gmail.com>
2023-05-26 16:06:10 +0200

    Add CI

---
+++ .ci/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl https://github.com/QB64Official/qb64/releases/download/v2.1/qb64_dev_2022-09-08-07-14-00_47f5044_lnx.tar.gz
+tar -xf qb64_dev_2022-09-08-07-14-00_47f5044_lnx.tar.gz
+mv qb64_dev_2022-09-08-07-14-00_47f5044_lnx qb64
+
