e5b8d1745c951b88965af2a6184d4f37db81b380
Lorenzo Castellino <castellino96@gmail.com>
2023-06-06 09:46:23 +0200

    Change CI dir

--- .ci/install.sh
+++ .ci/install.sh
@@ -3,4 +3,5 @@
 wget https://github.com/QB64Official/qb64/releases/download/v2.1/qb64_dev_2022-09-08-07-14-00_47f5044_lnx.tar.gz -O qb64.tar.gz
 tar -xf qb64.tar.gz
 mv qb64_2022-09-08-23-38-12_47f5044_lnx qb64
-
+cd qb64
+./setup_lnx.sh
