9bee47b8bbcbfb3ca4f19778d4424d9050b3e4a2
Lorenzo Castellino <castellino96@gmail.com>
2023-06-06 11:35:36 +0200

    Add multi os release

---
+++ .ci/install.cmd
@@ -0,0 +1,7 @@
+curl -L https://github.com/QB64Official/qb64/releases/download/v2.1/qb64_dev_2022-09-08-07-14-00_47f5044_win-x64.7z -o qb64.7z
+curl -L https://www.7-zip.org/a/7zr.exe -o 7zr.exe
+
+7zr.exe x qb64.7z
+
+cd qb64
+call setup_win.cmd
--- .ci/install.sh
+++ .ci/install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-wget https://github.com/QB64Official/qb64/releases/download/v2.1/qb64_dev_2022-09-08-07-14-00_47f5044_lnx.tar.gz -O qb64.tar.gz
+OS=$1
+
+if [[ $OS == "macos" ]]; then
+    curl -L https://github.com/QB64Official/qb64/releases/download/v2.1/qb64_dev_2022-09-08-07-14-00_47f5044_osx.tar.gz -o qb64.tar.gz
+else
+    wget https://github.com/QB64Official/qb64/releases/download/v2.1/qb64_dev_2022-09-08-07-14-00_47f5044_lnx.tar.gz -O qb64.tar.gz
+fi
 tar -xf qb64.tar.gz
-mv qb64_2022-09-08-23-38-12_47f5044_lnx qb64
+mv qb64_* qb64
 cd qb64
-./setup_lnx.sh
+
+if [[ $OS == "macos" ]]; then
+    ./setup_osx.command
+else
+    ./setup_lnx.sh
+fi
